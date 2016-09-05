c3a50f8448c4fa24be7bad5b1e73321011ad7b19
diegobckn <reposalgo3>
2016-09-05 12:26:50 -0300

    preparando modelos, migraciones, seed y factories

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -10,6 +10,8 @@
 | database. Just tell the factory how a default model should look.
 |
 */
+// use Faker\Factory as Faker;
+
 
 $factory->define(App\User::class, function (Faker\Generator $faker) {
     return [
@@ -17,5 +19,36 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
         'email' => $faker->safeEmail,
         'password' => bcrypt(str_random(10)),
         'remember_token' => str_random(10),
+    ];
+});
+
+
+
+$factory->define(App\Category::class, function (Faker\Generator $faker) {
+    return [
+        'name' => $faker->jobTitle,
+    ];
+});
+
+
+$factory->define(App\Tag::class, function (Faker\Generator $faker) {
+    return [
+        'name' => $faker->company,
+        'created_at'=>$faker->dateTimeBetween($startDate = '-30 days', $endDate = '-15 days'),
+        'updated_at'=>$faker->dateTimeBetween($startDate = '-14 days'),
+    ];
+});
+
+
+
+
+$factory->define(App\Post::class, function (Faker\Generator $faker) {
+    return [
+        'title' => $faker->title,
+        'body' => $faker->paragraph(2),
+        'user_id' => $faker->numberBetween(1,10),
+        'category_id' => $faker->numberBetween(1,10),
+        'created_at'=>$faker->dateTimeBetween($startDate = '-30 days', $endDate = '-15 days'),
+        'updated_at'=>$faker->dateTimeBetween($startDate = '-14 days'),
     ];
 });
